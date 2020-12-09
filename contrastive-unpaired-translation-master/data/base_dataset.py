ec0e4bc581d2838bb990a5504cc73cdc18ebe528
AgustinaLaGreca <agustina.lagrecas@gmail.com>
2020-12-09 16:55:01 +0100

    Update base_dataset.py
    Add data augmentation (still we could do elastic deformation)

--- contrastive-unpaired-translation-master/data/base_dataset.py
+++ contrastive-unpaired-translation-master/data/base_dataset.py
@@ -79,8 +79,10 @@ def get_params(opt, size):
     return {'crop_pos': (x, y), 'flip': flip}
 
 
-def get_transform(opt, params=None, grayscale=False, method=Image.BICUBIC, convert=True):
+def get_transform(opt, params=None, grayscale=False, data_augmentation = True, method=Image.BICUBIC, convert=True):
     transform_list = []
+    if data_augmentation:
+        transform_list.append(transforms.RandAffine(15., translate=0.15, shear=[2.,2.]))
     if grayscale:
         transform_list.append(transforms.Grayscale(1))
     if 'fixsize' in opt.preprocess:
