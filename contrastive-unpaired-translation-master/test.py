5730ce35f45410f4c029ccb01559e88a230526f2
maxmartinezruts <38012876+maxmartinezruts@users.noreply.github.com>
2021-01-11 19:25:05 +0100

    Update test.py

--- contrastive-unpaired-translation-master/test.py
+++ contrastive-unpaired-translation-master/test.py
@@ -97,7 +97,7 @@ if __name__ == '__main__':
             model.test()           # run inference
             time.sleep(.5)
             print(model.fake_B.shape)
-            fake_B = model.fake_B
+            fake_B = model.real_A
             patch.patch = fake_B.cpu().numpy()  # get image results
             print(patch.patch.shape)
         print(len(patches))
