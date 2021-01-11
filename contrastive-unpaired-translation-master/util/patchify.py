df7ea8164bf6b52747f21b32b0ff0e5fb5f5097e
maxmartinezruts <38012876+maxmartinezruts@users.noreply.github.com>
2021-01-11 16:51:51 +0100

    Update patchify.py

--- contrastive-unpaired-translation-master/util/patchify.py
+++ contrastive-unpaired-translation-master/util/patchify.py
@@ -50,9 +50,9 @@ def unpatchify(patches, crop, s):
 
     for patch in patches:
         l, r, t, b = patch.left + crop // 4, patch.right - crop // 4, patch.top + crop // 4, patch.bottom - crop // 4
-        img_reconstructed[:, :, t + 500:b + 500, l + 500:r + 500] = patch.patch[:, :, crop // 4:-crop // 4, crop // 4:-crop // 4]
+        img_reconstructed[:, :, t + 500:b + 500, l + 500:r + 500] += patch.patch[:, :, crop // 4:-crop // 4, crop // 4:-crop // 4]
         img_reconstructed_count[:, :, t + 500:b + 500, l + 500:r + 500] += 1
 
-    return img_reconstructed[:, :, 500:500 + s, 500:500 + s]
+    return img_reconstructed[:, :, 500:500 + s, 500:500 + s] /img_reconstructed_count[:, :, 500:500 + s, 500:500 + s]
 
 
