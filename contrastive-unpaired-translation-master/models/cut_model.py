3ffafedd3d18db8cb8c6230287659d60485e95b6
maxmartinezruts <38012876+maxmartinezruts@users.noreply.github.com>
2021-01-11 18:32:38 +0100

    Update cut_model.py

--- contrastive-unpaired-translation-master/models/cut_model.py
+++ contrastive-unpaired-translation-master/models/cut_model.py
@@ -146,10 +146,13 @@ class CUTModel(BaseModel):
     def forward(self):
         """Run forward pass; called by both functions <optimize_parameters> and <test>."""
         self.real =  self.real_A
-
+        if self.opt.flip_equivariance:
+            self.flipped_for_equivariance = self.opt.isTrain and (np.random.random() < 0.5)
+            if self.flipped_for_equivariance:
+                self.real = torch.flip(self.real, [3])
 
         self.fake = self.netG(self.real)
-        self.fake_B = self.fake[:self.real_A.size(0)]
+        self.fake_B = self.fake[0]
         if self.opt.nce_idt:
             self.idt_B = self.fake[self.real_A.size(0):]
 
