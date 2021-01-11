a49af5fa948873fcf313128a686bf877b1af85fe
maxmartinezruts <38012876+maxmartinezruts@users.noreply.github.com>
2021-01-11 21:36:04 +0100

    Update test.py

--- contrastive-unpaired-translation-master/test.py
+++ contrastive-unpaired-translation-master/test.py
@@ -93,8 +93,8 @@ if __name__ == '__main__':
 
                 patch = patches[p]
 
-                transposed = np.transpose(patch, (2,3))
-                data['A'] = torch.tensor(patch.patch.T).type(torch.FloatTensor)
+                transposed = np.transpose(patch.patch, (2,3))
+                data['A'] = torch.tensor(transposed).type(torch.FloatTensor)
                 model.set_input(data)  # unpack data from data loader
                 model.test()           # run inference
                 fake_B = model.fake_B.clone().detach()
