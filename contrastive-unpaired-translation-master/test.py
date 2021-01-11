cf11e9197b846855cb5fd33a86369d3674a589fa
maxmartinezruts <38012876+maxmartinezruts@users.noreply.github.com>
2021-01-11 18:23:10 +0100

    Update test.py

--- contrastive-unpaired-translation-master/test.py
+++ contrastive-unpaired-translation-master/test.py
@@ -94,7 +94,7 @@ if __name__ == '__main__':
             data['A'] = torch.tensor(patch.patch).type(torch.cuda.FloatTensor)
             model.set_input(data)  # unpack data from data loader
             model.test()           # run inference
-            print(model.get_current_visuals().keys())
+            print(model.fake_B.shape)
             fake_B = model.get_current_visuals()['real_A']
             patch.patch = fake_B.cpu().numpy()  # get image results
             print(patch.patch.shape)
