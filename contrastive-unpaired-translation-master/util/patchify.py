1dd2cf678c7bd576a3df5e8e7aa1680a309da5fd
maxmartinezruts <38012876+maxmartinezruts@users.noreply.github.com>
2021-01-13 11:35:52 +0100

    Update patchify.py

--- contrastive-unpaired-translation-master/util/patchify.py
+++ contrastive-unpaired-translation-master/util/patchify.py
@@ -58,5 +58,5 @@ def unpatchify(patches, crop, s):
     print('normal',img_reconstructed.shape)
     print('mean',np.nanmedian(img_reconstructed[:, :, 500:500 + s, 500:500 + s, :], axis=(4)).shape)
     matrix = img_reconstructed[:, :, 500:500 + s, 500:500 + s,:]
-    return np.ma.mean(np.where(matrix!=0,matrix,np.nan), axis=(4))
+    return np.mean(np.where(matrix!=0,matrix,np.nan), axis=(4))
 
