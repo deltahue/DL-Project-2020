75fb58945b5b8ad8436cfb16af37e7afcd125021
maxmartinezruts <38012876+maxmartinezruts@users.noreply.github.com>
2021-01-13 21:42:33 +0100

    s

--- contrastive-unpaired-translation-master/data/base_dataset.py
+++ contrastive-unpaired-translation-master/data/base_dataset.py
@@ -117,8 +117,9 @@ def get_transform(opt, params=None, grayscale=True, data_augmentation = True, me
 
     # if opt.preprocess == 'none':
     # transform_list.append(transforms.Lambda(lambda img: __make_power_2(img, base=4, method=method)))
-    if data_augmentation:
-        transform_list.append(transforms.RandomAffine(2.,scale = [0.9,1.1]))
+    if opt.isTrain:
+        if data_augmentation:
+            transform_list.append(transforms.RandomAffine(2.,scale = [0.9,1.1]))
     if not opt.no_flip:
         if params is None or 'flip' not in params:
             transform_list.append(transforms.RandomHorizontalFlip())
