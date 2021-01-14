27a63b57c4a4f41cc0f862ecd1e8afc687a5ba1d
maxmartinezruts <38012876+maxmartinezruts@users.noreply.github.com>
2021-01-14 14:52:15 +0100

    Update test.py

--- contrastive-unpaired-translation-master/test.py
+++ contrastive-unpaired-translation-master/test.py
@@ -113,7 +113,8 @@ if __name__ == '__main__':
 
         new_CT = 4095*prediction - 1024
         ni_image = nibabel.Nifti1Image(new_CT[0,0,:,:], np.eye(4))
-        nibabel.save(ni_image, 'content/results/'+img_path[0])
+        os.makedirs('content/results', exist_ok=True)
+        nibabel.save(ni_image, 'content/results'+img_path[0])
         prediction = (prediction-0.5)/0.5
         print(prediction)
         # prediction = np.array(scipy.ndimage.zoom(prediction, (1, 1, 2, 2), order=1),dtype=np.float)
