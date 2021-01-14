1b18d558371fdad637cfc681ffbb68a6ad386ce5
AgustinaLaGreca <agustina.lagrecas@gmail.com>
2021-01-14 13:43:39 +0100

    Add files via upload

--- contrastive-unpaired-translation-master/util/patchify.py
+++ contrastive-unpaired-translation-master/util/patchify.py
@@ -39,10 +39,10 @@ def patchify(img, n, patch_size):
     for i in range(n):
         for j in range(n):
                 patch = Patch(img, i, j, n, patch_size)  # Create patch
-                # C = np.count_nonzero(patch.patch)
-                # print(C, 256 * 256 / 4)
-                # if (n//3 <= i < 2*n//3 and n//3+2 <= j < 2*n//3) or (C > patch_size*patch_size/2) :
-                patches.append(patch)  # Add patch to list
+                C = np.count_nonzero(patch.patch)
+                print(C, 256 * 256 / 4)
+                if (n//3 <= i < 2*n//3 and n//3+2 <= j < 2*n//3) or (C > patch_size*patch_size/2) :
+                    patches.append(patch)  # Add patch to list
     return patches
 
 
@@ -55,13 +55,31 @@ def unpatchify(patches, crop, s):
         print(patch.patch.shape)
 
         l, r, t, b = patch.left + crop // 4, patch.right - crop // 4, patch.top + crop // 4, patch.bottom - crop // 4
-        img_reconstructed[:, :, t + 500:b + 500, l + 500:r + 500, p] = patch.patch[:,:,:,:]
+        img_reconstructed[:, :, t + 500:b + 500, l + 500:r + 500, p] = patch.patch
 
     print('normal',img_reconstructed.shape)
     print('mean',np.nanmedian(img_reconstructed[:, :, 500:500 + s, 500:500 + s, :], axis=(4)).shape)
     img_reconstructed_useful = img_reconstructed[:, :, 500:500 + s, 500:500 + s,:]
     y = np.ma.masked_where(img_reconstructed_useful == 0, img_reconstructed_useful)
     res = np.ma.median(y, axis=4).filled(0)
+
+    print(res.shape)
+    label_y = y.copy()
+    label_y[y < 0.2] = 1
+    label_y[y >= 0.2] = 2
+    label_y[y > 0.3] = 3
+    label_count = np.apply_along_axis(np.bincount, axis=4, arr=label_y,
+                                      minlength=np.max(label_y) + 1)
+    label_argmax = np.argmax(label_count, axis=-1)
+    average_y = res.copy()
+    for i in range(512):
+        for j in range(512):
+            array_patches = y[0,0,i,j,:]
+            label_y_patches = label_y[0,0,i,j,:]
+            label_count = np.apply_along_axis(np.bincount, axis=4, arr=array_patches,
+                                      minlength=np.max(array_patches) + 1)
+            label_argmax = np.argmax(label_count, axis=-1)
+            label_vote = label_argmax[0,0,i,j,0]
+            average_y[0,0,i,j] = np.mean(array_patches[label_y_patches == label_vote],axis = -1)
+    print(average_y.shape)
     return  res
-
-
