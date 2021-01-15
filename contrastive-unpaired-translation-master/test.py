24e39a1b4042b763715821f1ea546286491db7c0
maxmartinezruts <38012876+maxmartinezruts@users.noreply.github.com>
2021-01-15 11:41:42 +0100

    Update test.py

--- contrastive-unpaired-translation-master/test.py
+++ contrastive-unpaired-translation-master/test.py
@@ -94,7 +94,7 @@ if __name__ == '__main__':
         print('Input max', np.amax(real_A.numpy()))
         print('Input max', real_A.numpy().shape)
 
-        patches = patchify.patchify(real_A.numpy(),32, 256)
+        patches = patchify.patchify(real_A.numpy(),16, 256)
         for p in range(len(patches)):
 
             patch = patches[p]
